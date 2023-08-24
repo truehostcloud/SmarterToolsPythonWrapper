a611a71279f04e912fb77f2b2f9ce648dba23d69
Collins Lagat <cklagat@gmail.com>
2023-08-24 11:10:47 +0300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="smartermail-api",
+    version="0.3.0",
+    description="This is a simple python wrapper for the SmarterMail API.",
+    author="Zachary Sylvester",
+    license="Apache License 2.0",
+    packages=find_packages(include=["api"]),
+    install_requires=["requests"],
+    project_urls={
+        "Source": "https://github.com/zjs81/SmarterToolsPythonWrapper",
+    },
+)
